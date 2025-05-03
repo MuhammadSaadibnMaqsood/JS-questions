0027f626997cb81f0865efc356412eefb9998d87
Muhammad Saad <muhammadsaady0100@gmail.com>
2025-05-03 16:56:29 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+//  question #1
+
+// let count = 0;
+// arr = [true, false, true, false];
+// arr.map((item) => {
+//     if (item == true) {
+//         count++;
+//     }
+// })
+
+// Question #2
+
+// function input(str) {
+//     console.log(str);
+// }
+// function main() {
+//     input("Hey saad here");
+// }
+// main();
