a0ea6c9b550af676ff78db9ba7c180cff215b6f0
Muhammad Saad <muhammadsaady0100@gmail.com>
2025-05-04 13:52:48 -0700

    Question3-4

---
+++ Question3-4.js
@@ -0,0 +1,27 @@
+// question #3
+
+// let a =0;
+// let b= 5;
+
+// let dice = Math.floor(Math.random() * 6) + 1;
+
+// a= a+dice
+
+// if(a==b){
+//     console.log("true");
+
+// }
+// else{
+//     console.log("false");
+// }
+
+// question #4
+
+// function input(num){
+//     str = num.toString().length;
+//     int = parseInt(str)
+//     return int
+// }
+
+// const output = input(1000);
+// console.log(output);
