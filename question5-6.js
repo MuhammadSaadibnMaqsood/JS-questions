cd8cb371b782c72a9be95ea947c325cc59bcf079
Muhammad Saad <muhammadsaady0100@gmail.com>
2025-05-05 13:50:04 -0700

    Add question5-6.js

---
+++ question5-6.js
@@ -0,0 +1,45 @@
+// Question #5
+
+// function input(letter,num){
+
+//     letter === 'c'? radius = num * 6.28 : radius = num *4;
+//     console.log(radius);
+
+// }
+
+// input('s',4);
+// input('c',4);
+
+// Question #6
+
+// let houses = []
+// function name(arr){
+
+
+//     arr.map((item)=>{
+// item.length < 6? console.log(`${item} Gryffindor`) :'';
+// item.length < 8? console.log(`${item} huuflepuff`):'';
+// item.length < 12? console.log(`${item} Ravenclaw`):'';
+// item.length > 12? console.log(`${item} Slytherine`):'';
+
+//         if (item.length < 6) {
+//             houses.push(`${item} Gryffindor`);
+
+//         } else if (item.length < 8) {
+//             houses.push(`${item} huuflepuff`);
+
+//         }
+//          else if (item.length < 12) {
+//             houses.push(`${item} Ravenclaw`);
+//         }
+//         else{
+//             houses.push(`${item} Slytherine`);
+//         }
+
+//     })
+
+// }
+
+// const arr =["Saad","MisbahSaad","Farasat","asdadasdasdasdasdaddsa"];
+// name(arr);
+// console.log(houses);
