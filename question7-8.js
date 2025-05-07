5c66b05a2bdc2b5744969b5e93b3a33bb00c0eef
Muhammad Saad <muhammadsaady0100@gmail.com>
2025-05-06 18:48:40 -0700

    Add question7-8.js

---
+++ question7-8.js
@@ -0,0 +1,38 @@
+// Question #7
+
+// arr = ["Saad","Misbah","Moiz", "Farasat","Saad","Moiz"];
+// temp = []
+
+// function DoubleTrouble(arr){
+//     arr.map((item)=>{
+
+//         if(!temp.includes(item)){
+
+//             temp.push(item);
+//             arr.push(item);
+
+//         }
+
+
+//     }
+
+//     )
+//     console.log(arr);
+
+
+// }
+// DoubleTrouble(arr)
+
+
+// Question #8
+
+// const strings = "Muhammad Saad";
+
+// function reverse(string){
+
+//   let reverses = strings.split("").reverse().join("");;
+//     console.log(reverses);
+
+// }
+
+// reverse(strings);
