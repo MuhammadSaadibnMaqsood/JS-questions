e15c6a7e7c78d295f51ef03bd3bc16b39c1f60d1
Muhammad Saad <muhammadsaady0100@gmail.com>
2025-05-07 17:10:38 -0700

    Add question7-8.js

---
+++ question9-10.js
@@ -0,0 +1,47 @@
+// Question #9
+
+// function passwordValidator(pass){
+//     lowerCase = /[a-z]/.test(pass);
+//     upperCase = /[A-Z]/.test(pass);
+//     special = /[&@#%]/.test(pass);
+
+
+
+
+//     if (lowerCase && upperCase && special) {
+
+//         console.log("Pass is strong");
+
+//     }
+//     else {
+
+//         console.log("Pass is weak");
+
+//     }
+// }
+
+// let input = prompt("Enter pass");
+// passwordValidator(input);
+
+
+// Question #10
+
+// function sum(arr){
+//     result = 0
+//     arr.map((item,index)=>{
+//         if(item >= 0){
+//             result = item + result;
+//         }
+//         else if (item < 0 ){
+//             console.log(`${index} : Error negative num can anot be added`);
+
+//         }
+
+
+
+//     })
+//     console.log(result);
+// }
+
+// arr = [1,2,3,4,5,6,7,8,9,10,0,-1];
+// sum(arr);
