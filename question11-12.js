8c9b356db72c4b3d84cb6c9c69a2482fe1c8a6cd
Muhammad Saad <muhammadsaady0100@gmail.com>
2025-05-08 17:52:38 -0700

    Add question 11-12

---
+++ question11-12.js
@@ -0,0 +1,41 @@
+// Question #11
+
+// function Vowelcounter(str){
+//     count = 0;
+//     vowel = 'aeiou';
+//     temp = str.split('');
+
+//     temp.map((item)=>{
+//         if(vowel.includes(item)){
+//             count++;
+//         }
+
+//     })
+
+//     console.log(count);
+
+// }
+
+// Vowelcounter('Saad');
+
+// Question #12
+
+// function savetolocalstorage(obj){
+
+//     localStorage.setItem("obj",JSON.stringify(obj));
+
+// }
+// function getItem(){
+
+//     let a = JSON.parse(localStorage.getItem("obj"));
+//     console.log(a);
+
+// }
+
+// // obj = {name: "Saad", age: 21, occupation: "Undergraduate"};
+// let name = prompt("Enter you name");
+// let age = prompt("Enter you age");
+
+// obj = {name: name, age: age };
+// savetolocalstorage(obj);
+// getItem();
