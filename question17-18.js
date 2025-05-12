4f8df6b2fb53f04624ff70bbdf84976faad77d8d
Muhammad Saad <muhammadsaady0100@gmail.com>
2025-05-11 19:58:17 -0700

    Add question17-18

---
+++ question17-18.js
@@ -0,0 +1,25 @@
+// question #17
+
+// function findmax(max){
+//     let maxi = 0
+//     for(let i =0;i<=max.length; i++){
+//         if(max[i]> maxi){
+//             maxi = max[i]
+//         }
+//     }
+//     return maxi
+// }
+// max = [2,3,4,5,69,10];
+// console.log(findmax(max));
+
+// question #18
+
+// function removeFirstElement(arr){
+//     newArr = arr.slice(1);
+//     newArr2 = arr.slice(1,3);
+//     return [newArr,newArr2]
+// }
+// arr = [2,3,4,5,69,10];
+// let [arr2,arr3] = removeFirstElement(arr)
+// console.log(arr2);
+// console.log(arr3);
