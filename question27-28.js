84ba8956aeed8b31ccaced507fa113ff1692547e
Muhammad Saad <muhammadsaady0100@gmail.com>
2025-05-17 02:53:56 -0700

    added question27-28

---
+++ question27-28.js
@@ -0,0 +1,50 @@
+// question #27
+
+// Implement a deep clone function in JavaScript that creates a copy of a nested object or array without any reference to the original.
+
+
+// const original = {
+//     Name: "Saad",
+//     Age: 21,
+//     Skills: ['JS', 'React', 'Express'],
+//     Qualification: "Undergraduate"
+// }
+
+
+
+// const deepCopy = JSON.parse(JSON.stringify(original));
+
+
+
+// deepCopy.Skills.push('Nodejs');
+
+// console.log(deepCopy);
+// console.log(original);
+
+
+// question #28
+
+// Write a recursive function to calculate the factorial of a given number.
+
+// function factorail(arr) {
+//     if (arr == 1) {
+//         return arr
+//     } else if (arr == 2) {
+//         return arr
+//     }
+//     else if (arr > 2) {
+
+//         let fac = 1;
+
+//         for (let i = 1; i <= arr; i++) {
+
+
+//             fac = i * fac;
+
+
+//         }
+//         return fac
+//     }
+// }
+// arr = [5];
+// console.log(factorail(arr));
