edd9c4a22451c83bf430b9f2d5ddc921fdf8d3e5
Muhammad Saad <muhammadsaady0100@gmail.com>
2025-05-19 10:35:14 -0700

    Added question 31-32

---
+++ question31-32.js
@@ -0,0 +1,39 @@
+// Question #31
+
+// Flatten nested array
+
+// arr = [1,2,[3,4,[6]]];
+
+// function flattenarr(arr){
+//     let flattenarr = arr.flat(Infinity);
+//     console.log(flattenarr);
+// }
+// flattenarr(arr);
+
+// Question #32
+
+// Write a function that determines if two strings are anagrams of each other
+
+// let string1 = "Saad"
+// let string2 = "ahmed"
+
+// function checkanagram (str1,str2){
+//     let str1update = str1.toLowerCase().split('').sort().join('');
+//     let str2update = str2.toLowerCase().split('').sort().join('');
+
+
+
+
+
+//     if(str1update === str2update){
+
+//         console.log("Strings are anagrams");
+
+//     }else{
+
+//         console.log("Strings are not anagrams");
+
+//     }
+// }
+
+// checkanagram(string1,string2);
