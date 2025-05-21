b9e9cfdefd0343b8484e77609f76dfe66c54f68a
Muhammad Saad <muhammadsaady0100@gmail.com>
2025-05-21 01:35:56 -0700

    Added question 33 - 24

---
+++ question33-34.js
@@ -0,0 +1,29 @@
+// Question #33
+
+// Implement a function to reverse a string without using the built-in reverse() method.
+
+// function reverseString(str) {
+//     split = str.split("");
+//     let reverse = []
+//     for (let i = split.length; i >= 0; i--) {
+
+//         reverse.push(split[i]);
+
+
+//     }
+//     console.log(reverse.join(''));
+// }
+// reverseString("Saad");
+
+// Question #34
+
+// Given an array of numbers, write a function to find the largest and smallest numbers in the array.
+
+// const arr = [2, 4, 6, 8, 10, 3];
+
+// function findMinAndMax(arr) {
+//     console.log("Maximum num in array is: ", Math.max(...arr));
+//     console.log("Minimum num in array is: ", Math.min(...arr));
+
+// }
+// findMinAndMax(arr);
