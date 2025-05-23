baafdf2ff3c3f90d4f199808771954ac9f6e0011
Muhammad Saad <muhammadsaady0100@gmail.com>
2025-05-23 10:39:35 -0700

    Added question 35-36.js

---
+++ question35-36.js
@@ -0,0 +1,37 @@
+// Question #35
+
+// Write a function that takes an array of integers as input and returns a new array with only the unique elements.
+
+// function uniqueElements(arr) {
+
+//     let unique = [];
+
+//     arr.map((item) => {
+//         if (!unique.includes(item)) {
+//             unique.push(item)
+//         }
+//     })
+
+//     console.log(unique);
+
+
+// }
+// const arr = [2, 4, 6, 8, 10, 3, 2, 4, 3];
+// uniqueElements(arr)
+
+// Question #36
+
+// Write a function that determines if a given number is prime or not.
+
+// function primeOrNot(num){
+//     if(num % 2 == 0 || num == 0){
+//         console.log("Num is prime");
+
+//     }
+//     else{
+//         console.log("Num is not prime");
+
+//     }
+// }
+
+// primeOrNot(0);
