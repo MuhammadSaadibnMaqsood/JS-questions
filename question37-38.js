46274932bae752b212c1745ea487ba55a8ab208e
Muhammad Saad <muhammadsaady0100@gmail.com>
2025-05-25 06:36:28 -0700

    Added question37-38.js

---
+++ question37-38.js
@@ -0,0 +1,44 @@
+// Question #37
+
+// Implement a function to find the sum of all the numbers in an array.
+
+// function findSum(arr){
+//     let sum = 0;
+
+//     arr.map((item)=>{
+//         sum = sum + item
+//     })
+
+//     console.log(sum)
+// }
+
+// const arr = [2,3,4];
+
+// findSum(arr);
+
+// Question #38
+
+// Given a string, write a function to count the occurrences of each character in the string.
+
+// function CountEachCharacter(Str){
+
+//     // let split = str.split('');
+//     let charCount = {}
+
+//     for(let char of str){
+//          if(charCount[char]){
+//             charCount[char]++;
+//          }
+//          else{
+//             charCount[char] = 1;
+//          }
+//     }
+
+//     console.log(charCount);
+
+
+
+// }
+// let str = "Hey What are you doing man"
+
+// CountEachCharacter(str);
