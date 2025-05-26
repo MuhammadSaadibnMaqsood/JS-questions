bd34366ad33d4d2fa12bec19e0678d3064f89364
Muhammad Saad <muhammadsaady0100@gmail.com>
2025-05-26 01:44:10 -0700

    Added question39-40

---
+++ question39-40.js
@@ -0,0 +1,29 @@
+// Quesion #39
+
+// Implement a function to remove duplicates from an array.
+
+// function removeDublicate(arr){
+//     let unique = [];
+//     arr.map((item)=>{
+//         if(!unique.includes(item)){
+//             unique.push(item);
+//         }
+
+//     })
+//     console.log(unique);
+
+// }
+// let arr = [1,1,1,2,3,4,5,5,6,7,8,8];
+// removeDublicate(arr);
+
+// Question #40
+
+// Write a function that sorts an array of numbers in ascending order.
+
+// function sortinAccending(arr){
+//     console.log(arr.sort());
+
+// }
+// let arr = [6,7,8,1,2,3,4,5,9];
+
+// sortinAccending(arr);
