cad42fb127bebc71d718bd63bff4472170085142
Muhammad Saad <muhammadsaady0100@gmail.com>
2025-05-27 15:28:19 -0700

    Added question41-42.js

---
+++ question41-42.js
@@ -0,0 +1,42 @@
+// Question #41
+
+// Write a function that reverses the order of words in a sentence without using the built-in reverse() method.
+
+// function reverseSentence(sentence) {
+
+//     let split = sentence.split(/[ t]/);
+//     // console.log(split);
+//     let reverse = [];
+
+//     for (let i = split.length-1; i >= 0; i--) {
+//         reverse.push(split[i]);
+//     }
+//     console.log(reverse);
+
+
+
+// }
+
+// let sentence = "Hello my name is Saad"
+
+// reverseSentence(sentence);
+
+// Question #42
+
+// Write a function that takes an array of integers and returns the largest difference between any two numbers in the array.
+
+// function findLargestDiff(arr) {
+
+
+//     const max = Math.max(...arr);
+//     const min = Math.min(...arr);
+
+
+
+//     const result = max - min;
+
+//     console.log("The highest difference value is: ", result);
+
+// }
+// let arr = [2, 3, 4, 5, 6, 7, 8, 9];
+// findLargestDiff(arr);
