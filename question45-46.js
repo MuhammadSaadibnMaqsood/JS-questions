a0cf93f67ac0f97ce1cccd9c34798d8069140486
Muhammad Saad <muhammadsaady0100@gmail.com>
2025-05-29 02:03:42 -0700

    Added question45-46

---
+++ question45-46.js
@@ -0,0 +1,30 @@
+// Question #45
+
+//  Implement a function that returns the average value of numbers in an array.
+
+// function findAverage(arr) {
+//     let sum = 0;
+//     arr.map((item) => {
+//         sum = sum + item;
+//     });
+
+//     const average = sum / arr.length;
+//     console.log(average);
+
+
+
+// }
+// arr = [2, 3, 2, 6, 1];
+// findAverage(arr);
+
+// Question #46
+
+// Write a function that sorts an array of strings in alphabetical order.
+
+// function sortAlphabetical(arr){
+//     console.log(arr.sort());
+
+// }
+// const arr = ['Saad','Moiz','Ahmed'];
+
+// sortAlphabetical(arr);
