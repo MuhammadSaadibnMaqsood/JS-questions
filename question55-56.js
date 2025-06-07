5ed85baa2d4c70f9aadf5aec7d24c4051ffe69b4
Muhammad Saad <muhammadsaady0100@gmail.com>
2025-06-07 12:51:49 -0700

    Added question 55-56

---
+++ question55-56.js
@@ -0,0 +1,40 @@
+// Question #55
+
+        // Count num of frequency in an array 
+
+        // const arr = ['Apple', 'Mango', 'Apple', 'Apple', 'Mango', 'Apple', 'Banana'];
+        // let obj = {};
+        // function numOfFrequency(arr) {
+
+        //     arr.map((item) => {
+        //         if (obj[item]) {
+        //             obj[item] += 1
+
+        //         }
+        //         else {
+        //             obj[item] = 1
+        //         }
+        //     })
+        //     console.log(obj);
+            
+
+        // }
+        // numOfFrequency(arr)
+
+ // Question #56
+
+        // remove dublicate without using sets 
+
+        // const arr = [1, 2, 3, 2, 4, 1, 5, 3];
+        // let temp = []
+        // function removeDuplicate(arr){
+        //     arr.map((item)=>{
+        //         if(!temp.includes(item)){
+        //             temp.push(item)
+        //         }
+        //     })
+        //     console.log(temp);
+            
+
+        // }
+        // removeDuplicate(arr);f
